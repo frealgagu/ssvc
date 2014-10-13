54fa453d072d78d982805647815e3629cd976168
frealgagu <frealgagu@gmail.com>
2014-10-13 18:38:29 -0500

    Temperature position changed respect to Pressure position. (In order to show like PLC display)

--- src/main/java/co/edu/udistrital/view/status/LastRecordsWindow.java
+++ src/main/java/co/edu/udistrital/view/status/LastRecordsWindow.java
@@ -71,8 +71,8 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 
 	private InitApplication initApplication;
 	
-	private DataSeries pressureSeries;
 	private DataSeries temperatureSeries;
+    private DataSeries pressureSeries;
 	
 	/*- VaadinEditorProperties={"grid":"RegularGrid,20","showGrid":true,"snapToGrid":true,"snapToObject":true,"movingGuides":false,"snappingDistance":10} */
 	
@@ -159,57 +159,6 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
         MeasureService measureService = ApplicationServices.getMeasureService();
         Integer value = (Integer)radioFrequency.getValue();
 
-        List<Interval> pressureIntervals;
-        switch(value) {
-            case BY_SECONDS:
-                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureSecondIntervals(5));
-                break;
-            case BY_MINUTES:
-                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureMinuteIntervals(5));
-                break;
-            case BY_HOURS:
-                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureHourIntervals(5));
-                break;
-            default:
-                throw new IllegalArgumentException("No frequency selected");
-        }
-        Collections.reverse(pressureIntervals);
-
-        if(pressureIntervals.size() > 0) {
-            //Remove items which oversize the intervals
-            while(pressureSeries.size() > pressureIntervals.size()) {
-                pressureSeries.remove(pressureSeries.get(0));
-            }
-            Interval pressureInterval = pressureIntervals.get(0);
-            for(DataSeriesItem pressureItem : new ArrayList<>(pressureSeries.getData())) {
-                //Remove each item whose date is earlier to first interval
-                if(Util.toHighchartsTS(pressureInterval.getDateTime().toDate()) > pressureItem.getX().longValue()) {
-                    pressureSeries.remove(pressureItem);
-                }
-            }
-        }
-        for(int i = 0; i < pressureIntervals.size(); i++) {
-            Interval pressureInterval = pressureIntervals.get(i);
-            if(pressureSeries.size() > i) {
-                DataSeriesItem pressureItem = pressureSeries.get(i);
-                boolean update = false;
-                if(Util.toHighchartsTS(pressureInterval.getDateTime().toDate()) != pressureItem.getX().longValue()) {
-                    pressureItem.setX(pressureInterval.getDateTime().toDate());
-                    update = true;
-                }
-                if(pressureInterval.getValue().compareTo((BigDecimal)pressureItem.getY()) != 0) {
-                    pressureItem.setY(pressureInterval.getValue());
-                    update = true;
-                }
-                if(update) {
-                    pressureSeries.update(pressureItem);
-                }
-            } else {
-                DataSeriesItem pressureItem = new DataSeriesItem(pressureInterval.getDateTime().toDate(), pressureInterval.getValue());
-                pressureSeries.add(pressureItem);
-            }
-        }
-
         List<Interval> temperatureIntervals;
         switch(value) {
             case BY_SECONDS:
@@ -261,17 +210,68 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
             }
         }
 
+        List<Interval> pressureIntervals;
+        switch(value) {
+            case BY_SECONDS:
+                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureSecondIntervals(5));
+                break;
+            case BY_MINUTES:
+                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureMinuteIntervals(5));
+                break;
+            case BY_HOURS:
+                pressureIntervals = new ArrayList<>(measureService.retrieveLastPressureHourIntervals(5));
+                break;
+            default:
+                throw new IllegalArgumentException("No frequency selected");
+        }
+        Collections.reverse(pressureIntervals);
+
+        if(pressureIntervals.size() > 0) {
+            //Remove items which oversize the intervals
+            while(pressureSeries.size() > pressureIntervals.size()) {
+                pressureSeries.remove(pressureSeries.get(0));
+            }
+            Interval pressureInterval = pressureIntervals.get(0);
+            for(DataSeriesItem pressureItem : new ArrayList<>(pressureSeries.getData())) {
+                //Remove each item whose date is earlier to first interval
+                if(Util.toHighchartsTS(pressureInterval.getDateTime().toDate()) > pressureItem.getX().longValue()) {
+                    pressureSeries.remove(pressureItem);
+                }
+            }
+        }
+        for(int i = 0; i < pressureIntervals.size(); i++) {
+            Interval pressureInterval = pressureIntervals.get(i);
+            if(pressureSeries.size() > i) {
+                DataSeriesItem pressureItem = pressureSeries.get(i);
+                boolean update = false;
+                if(Util.toHighchartsTS(pressureInterval.getDateTime().toDate()) != pressureItem.getX().longValue()) {
+                    pressureItem.setX(pressureInterval.getDateTime().toDate());
+                    update = true;
+                }
+                if(pressureInterval.getValue().compareTo((BigDecimal)pressureItem.getY()) != 0) {
+                    pressureItem.setY(pressureInterval.getValue());
+                    update = true;
+                }
+                if(update) {
+                    pressureSeries.update(pressureItem);
+                }
+            } else {
+                DataSeriesItem pressureItem = new DataSeriesItem(pressureInterval.getDateTime().toDate(), pressureInterval.getValue());
+                pressureSeries.add(pressureItem);
+            }
+        }
+
         //chart.drawChart();
     }
 
 	private Configuration createInitialConfiguration() {
 		Configuration configuration = new Configuration();
         configuration.getChart().setType(ChartType.LINE);
-        configuration.getChart().setMarginRight(80);
+        configuration.getChart().setMarginRight(105);
         configuration.getChart().setMarginBottom(25);
 
-        configuration.getTitle().setText("Gr\u00E1fica de Presi\u00F3n y Temperatura");
-        configuration.getSubTitle().setText("Gr\u00E1fica de Presi\u00F3n y Temperatura");
+        configuration.getTitle().setText("Gr\u00E1fica de Temperatura y Presi\u00F3n");
+        configuration.getSubTitle().setText("Gr\u00E1fica de Temperatura y Presi\u00F3n");
 
         Axis xAxis = configuration.getxAxis();
         xAxis.setAllowDecimals(false);
@@ -279,7 +279,7 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 
         Axis yAxis = configuration.getyAxis();
         yAxis.setMin(0);
-        yAxis.setTitle(new Title("Presi\u00F3n / Temperatura"));//Libras por pulgada cuadrada (Pounds per Square Inch)
+        yAxis.setTitle(new Title("Temperatura / Presi\u00F3n"));
         yAxis.getTitle().setVerticalAlign(VerticalAlign.HIGH);
 
         configuration.getTooltip().setFormatter("'' + this.series.name + ': ' + this.y + (this.series.name == 'Temperatura' ? ' °C (Grados Celcius)' : ' psi (Libras por Pulgada Cuadrada)')");
@@ -301,19 +301,19 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 		Collections.reverse(pressureIntervals);
 		List<Interval> temperatureIntervals = new ArrayList<Interval>(measureService.retrieveLastTemperatureSecondIntervals(5));
 		Collections.reverse(temperatureIntervals);
-		pressureSeries = new DataSeries();
-		pressureSeries.setName("Presi\u00F3n");
 		temperatureSeries = new DataSeries();
 		temperatureSeries.setName("Temperatura");
+        pressureSeries = new DataSeries();
+        pressureSeries.setName("Presi\u00F3n");
 
-		for(Interval pressureInterval : pressureIntervals) {
-			pressureSeries.add(new DataSeriesItem(pressureInterval.getDateTime().toDate(), pressureInterval.getValue()));
-		}
 		for(Interval temperatureinterval : temperatureIntervals) {
 			temperatureSeries.add(new DataSeriesItem(temperatureinterval.getDateTime().toDate(), temperatureinterval.getValue()));
 		}
-        configuration.addSeries(pressureSeries);
+        for(Interval pressureInterval : pressureIntervals) {
+            pressureSeries.add(new DataSeriesItem(pressureInterval.getDateTime().toDate(), pressureInterval.getValue()));
+        }
         configuration.addSeries(temperatureSeries);
+        configuration.addSeries(pressureSeries);
         return configuration;
 	}
 
@@ -353,8 +353,7 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 		// btnControl
 		btnControl = new NativeButton();
 		btnControl.setCaption("Control PLC");
-		btnControl
-				.setIcon(new ThemeResource("img/plc-registers-settings-icon-72x72.png"));
+		btnControl.setIcon(new ThemeResource("img/plc-registers-settings-icon-72x72.png"));
 		btnControl.setImmediate(true);
 		btnControl.setWidth("86px");
 		btnControl.setHeight("86px");
@@ -363,8 +362,7 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 		// btnConfiguration
 		btnConfiguration = new NativeButton();
 		btnConfiguration.setCaption("Configuraci\u00F3n");
-		btnConfiguration.setIcon(new ThemeResource(
-				"img/settings-icon-72x72.png"));
+		btnConfiguration.setIcon(new ThemeResource("img/settings-icon-72x72.png"));
 		btnConfiguration.setImmediate(true);
 		btnConfiguration.setWidth("86px");
 		btnConfiguration.setHeight("86px");
@@ -373,8 +371,7 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 		// btnCurrentStatus
 		btnCurrentStatus = new NativeButton();
 		btnCurrentStatus.setCaption("Estado actual");
-		btnCurrentStatus.setIcon(new ThemeResource(
-				"img/current-status-icon-72x72.png"));
+		btnCurrentStatus.setIcon(new ThemeResource("img/current-status-icon-72x72.png"));
 		btnCurrentStatus.setImmediate(true);
 		btnCurrentStatus.setWidth("86px");
 		btnCurrentStatus.setHeight("86px");
@@ -383,8 +380,7 @@ public class LastRecordsWindow extends CustomComponent implements RefreshListene
 		// btnMemoryMap
 		btnMemoryMap = new NativeButton();
 		btnMemoryMap.setCaption("Mapa de memoria");
-		btnMemoryMap
-				.setIcon(new ThemeResource("img/memory-map-icon-72x72.png"));
+		btnMemoryMap.setIcon(new ThemeResource("img/memory-map-icon-72x72.png"));
 		btnMemoryMap.setImmediate(true);
 		btnMemoryMap.setWidth("86px");
 		btnMemoryMap.setHeight("86px");
